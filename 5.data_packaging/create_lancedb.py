f5956c02013faaae77a7d8ddae3f10a6eabe936c
d33bs <dave.bunten@cuanschutz.edu>
2024-09-16 16:42:57 -0600

    update docker; restructure isolated functions

--- 5.data_packaging/create_lancedb.py
+++ 5.data_packaging/create_lancedb.py
@@ -4,49 +4,14 @@ Python module for packaging data in lancedb database.
 
 import pathlib
 
-import duckdb
 import lancedb
-import pandas as pd
-import pyarrow as pa
-from constants import DATA_FILES_W_COLNAMES, PACKAGING_DATASETS
+from packager.constants import DATA_FILES_W_COLNAMES, PACKAGING_DATASETS
+from packager.tables import get_arrow_tbl_from_csv
 from pyarrow import parquet
 
 # specify a dir where the lancedb database may go and create lancedb client
 lancedb_dir = pathlib.Path("5.data_packaging/packaged/lancedb/mitocheck_data")
 ldb = lancedb.connect(lancedb_dir)
-
-
-def get_arrow_tbl_from_csv(filename_read: str) -> str:
-    """
-    Get an Arrow table from a CSV file through DuckDB.
-
-    Args:
-        filename_read (str):
-            The path to the CSV file to be read.
-
-    Returns:
-        str:
-            A string representing the Arrow table obtained from the CSV file.
-
-    """
-
-    # try to read a typed arrow table
-    # falling back to a high-memory (string-focused) pandas
-    # dataframe read converted to arrow
-    try:
-        with duckdb.connect() as ddb:
-            return ddb.execute(
-                f"""
-                SELECT *
-                FROM read_csv('{filename_read}');
-                """
-            ).arrow()
-    except duckdb.duckdb.ConversionException:
-        return pa.Table.from_pandas(
-            df=pd.read_csv(filepath_or_buffer=filename_read, low_memory=False),
-        )
-    except:
-        raise
 
 
 # send csv file data as arrow tables to a lancedb database
---
+++ 5.data_packaging/packager/tables.py
@@ -0,0 +1,90 @@
+"""
+Module for working with tables and schema of tables.
+"""
+
+import duckdb
+import pandas as pd
+import pyarrow as pa
+
+def get_arrow_tbl_from_csv(filename_read: str) -> str:
+    """
+    Get an Arrow table from a CSV file through DuckDB.
+
+    Args:
+        filename_read (str):
+            The path to the CSV file to be read.
+
+    Returns:
+        str:
+            A string representing the Arrow table obtained from the CSV file.
+
+    """
+
+    # try to read a typed arrow table
+    # falling back to a high-memory (string-focused) pandas
+    # dataframe read converted to arrow
+    try:
+        with duckdb.connect() as ddb:
+            return ddb.execute(
+                f"""
+                SELECT *
+                FROM read_csv('{filename_read}');
+                """
+            ).arrow()
+    except duckdb.duckdb.ConversionException:
+        return pa.Table.from_pandas(
+            df=pd.read_csv(filepath_or_buffer=filename_read, low_memory=False),
+        )
+    except:
+        raise
+
+def write_schema_str_to_file(filename_write: str, schema: str) -> str:
+    """
+    Write a schema string to a file.
+    """
+
+    with open(file=filename_write, mode="w", encoding="utf-8") as file:
+        file.write(schema)
+
+    return filename_write
+
+
+def get_arrow_schema_str_from_csv(filename_read: str) -> str:
+    """
+    Get an Arrow schema from a CSV file through DuckDB.
+
+    Args:
+        filename_read (str):
+            The path to the CSV file to be read.
+
+    Returns:
+        str:
+            A string representing the Arrow schema obtained from the CSV file.
+
+    """
+
+    # try to read a typed arrow table
+    # falling back to a high-memory (string-focused) pandas
+    # dataframe read converted to arrow
+    try:
+        with duckdb.connect() as ddb:
+            return (
+                ddb.execute(
+                    f"""
+                    SELECT *
+                    FROM read_csv('{filename_read}');
+                    """
+                )
+                .arrow()
+                .schema.to_string()
+            )
+    except duckdb.duckdb.ConversionException:
+        return pa.Table.from_pandas(
+            df=pd.read_csv(filepath_or_buffer=filename_read, low_memory=False),
+        ).schema.to_string()
+    except:
+        raise
+
+
+
+
